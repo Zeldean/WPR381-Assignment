a9f4756c6a7b0028eceddc9cf5c908d797d2d44a
Zeldean <deanvanzyl2004@gmail.com>
2025-05-07 19:14:57 +0200

    Add partials for header and footer, implement routing for pages.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // app.js
 
+// Import required modules
 const express = require("express");
 const path = require("path");
 const pageRoutes = require("./routes/pageRoutes");
@@ -7,12 +8,17 @@ const pageRoutes = require("./routes/pageRoutes");
 const app = express();
 const port = 3000;
 
+// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
+
+// Set EJS as the template engine and configure views directory
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// Use pageRoutes for handling requests to the root URL
 app.use("/", pageRoutes);
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
