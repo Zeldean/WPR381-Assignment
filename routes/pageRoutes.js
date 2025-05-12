007fbd4ead1c4c13a522dc257cf770f78664bfb1
Zeldean <deanvanzyl2004@gmail.com>
2025-05-12 22:43:31 +0200

    Extracted event data manegement and made the date more detailed and dynamic.

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,31 +1,14 @@
-// dummy Data
-const fs = require('fs').promises;
-const path = require('path');
-let events = []
-const eventsData = path.join(__dirname, "../data/events.json");
-
-fs.readFile(eventsData)
-.then( (data) => {
-    events = JSON.parse(data)
-    events = events.sort((a, b) => {
-        return new Date(b.startDate) - new Date(a.startDate);
-    });
-})
-.catch ( (err) => {
-    console.log(`${err}`);
-    events = []
-})
-
-
-
-
 // routes/pageRoutes.js
 
 const express = require('express');
 const router = express.Router();
+const em = require('../utils/eventManager.js')
 
-router.get('/', (req, res) => {
-    res.render('pages/home', { events: events, cssFile: "home.css" })
+
+
+router.get('/', async (req, res) => {
+    const events = await em.getEvents();
+    res.render('pages/home', { events: events, displayDate: em.displayDate, cssFile: "home.css" })
 });
 
 router.get('/about', (req, res) => {
@@ -33,7 +16,7 @@ router.get('/about', (req, res) => {
 });
 
 router.get('/events', (req, res) => {
-    res.render('pages/events', { events: events, cssFile: "events.css" })
+    res.render('pages/events', { events: events, displayDate: em.displayDate, cssFile: "events.css" })
 });
 
 router.get('/contact', (req, res) => {
