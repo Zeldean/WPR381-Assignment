1f555f10e06c2282763b6da722a6bdc19bb6c487
Zeldean <deanvanzyl2004@gmail.com>
2025-05-12 16:06:36 +0200

    Added home page with genaration for events. Added styling for home page. Updated header to allow for custom css.

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,10 +1,18 @@
+// dummy Data
+const events = [
+    { id: 1, title: 'Coding Bootcamp', date: '2025-05-20', location: 'Online', image: '/images/event1.jpg' },
+    { id: 2, title: 'Hackathon 2025', date: '2025-06-15', location: 'Campus', image: '/images/event2.jpg' },
+    { id: 3, title: 'Tech Talks', date: '2025-07-10', location: 'Online', image: '/images/event3.jpg' },
+    { id: 4, title: 'AI Workshop', date: '2025-08-22', location: 'Campus', image: '/images/event4.jpg' },
+];
+
 // routes/pageRoutes.js
 
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('pages/home')
+    res.render('pages/home', { events: events, cssFile: "home.css" })
 });
 
 router.get('/about', (req, res) => {
