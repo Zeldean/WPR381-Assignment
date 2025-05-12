0c443a238b8e282e1ef5f88d53ac931b20a2d595
Zeldean <deanvanzyl2004@gmail.com>
2025-05-12 19:56:35 +0200

    Added dynmicly disply of events on home page. Added json data for events

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,9 +1,9 @@
 // dummy Data
 const events = [
-    { id: 1, title: 'Coding Bootcamp', date: '2025-05-20', location: 'Online', image: '/images/event1.jpg' },
-    { id: 2, title: 'Hackathon 2025', date: '2025-06-15', location: 'Campus', image: '/images/event2.jpg' },
-    { id: 3, title: 'Tech Talks', date: '2025-07-10', location: 'Online', image: '/images/event3.jpg' },
-    { id: 4, title: 'AI Workshop', date: '2025-08-22', location: 'Campus', image: '/images/event4.jpg' },
+    { id: 1, title: 'Coding Bootcamp', date: '2025-05-20', location: 'Online', image: '01.jpg' },
+    { id: 2, title: 'Hackathon 2025', date: '2025-06-15', location: 'Campus', image: '02.jpg' },
+    { id: 3, title: 'Tech Talks', date: '2025-07-10', location: 'Online', image: '03.jpg' },
+    { id: 4, title: 'AI Workshop', date: '2025-08-22', location: 'Campus', image: '04.jpg' },
 ];
 
 // routes/pageRoutes.js
@@ -20,7 +20,7 @@ router.get('/about', (req, res) => {
 });
 
 router.get('/events', (req, res) => {
-    res.render('pages/events', { cssFile: "events.css" })
+    res.render('pages/events', { events: events, cssFile: "events.css" })
 });
 
 router.get('/contact', (req, res) => {
